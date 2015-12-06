fa64a851bdea6aaefd32562ad11f8dd9cefa87b4
Steve <champagst@gmail.com>
2015-12-06 17:38:12 -0600

    Add vim

--- install.sh
+++ install.sh
@@ -12,3 +12,5 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Bunch of symlinks
 
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
