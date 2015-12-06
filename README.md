b2026a99d9a159660693b524544ed4f14575464b
Steve <champagst@gmail.com>
2015-12-05 21:47:31 -0600

    Added more

---
+++ README.md
@@ -0,0 +1,18 @@
+# .files
+
+My dotfiles.
+
+## Install
+
+    sudo softwareupdate -i -a 
+    brew install coreutils
+
+### Clone with Git
+
+    git clone https://github.com/ggeise/dotfiles.git
+    source dotfiles/install.sh
+
+## Credits
+
+The majority of this is copied from [webpro](http://github.com/webpro/dotfiles/)
+My vimrc largely comes from [sjl](http://bitbucket.org/sjl/dotfiles/src/tip/vim/)
--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
 
-[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+# [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Bunch of symlinks
 
-ln -sfv "$DOTFILES_DIR/pentadactyl/.pentadactylrc" ~
+ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
