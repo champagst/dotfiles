12c6936ee983af78b8de7ecd7e61431d88b87596
Steve <champagst@gmail.com>
2015-12-05 19:24:32 -0600

    Inital commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Bunch of symlinks
+
+ln -sfv "$DOTFILES_DIR/pentadactyl/.pentadactylrc" ~
